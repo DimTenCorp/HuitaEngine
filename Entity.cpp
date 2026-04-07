756fb92931a5cd36e2a94f4070c5ef7c4b1125a9
dimashaa10 <dimashaa10@hotmail.com>
2026-04-08 01:21:30 +0700

    ббббббб

--- Entity.cpp
+++ Entity.cpp
@@ -1,18 +1,5 @@
 #include "Entity.h"
 
-// Конструктор по умолчанию
-CBaseEntity::CBaseEntity() 
-    : position(glm::vec3(0.0f, 0.0f, 0.0f)),
-      rotation(glm::vec3(0.0f, 0.0f, 0.0f)),
-      scale(glm::vec3(1.0f, 1.0f, 1.0f)),
-      isActive(true) {
-}
-
-// Деструктор
-CBaseEntity::~CBaseEntity() {
-    // Очистка ресурсов если необходима
-}
-
 // Обновление состояния сущности
 void CBaseEntity::Update(float deltaTime) {
     // Базовая логика обновления
